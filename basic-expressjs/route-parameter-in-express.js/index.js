5d4acdaae61ed4d35a0ec90c9accfdda8aa150e9
mrmuhammadrifki <immuhammadrifki@gmail.com>
2024-01-04 11:49:11 +0700

    Route Parameters in Express.Js

---
+++ basic-expressjs/route-parameter-in-express.js/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('This is home page');
+});
+
+app.get('/transactions', (req, res) => {
+  res.send('This is transactions page');
+});
+
+app.post('/transactions', (req, res) => {
+  res.send('This is transactions page from post');
+});
+
+app.get('/about', (req, res) => {
+  res.send('This is about page');
+});
+
+app.get('/article/:title', (req, res) => {
+  const { title } = req.params;
+
+  res.send(`You are reading article with title : ${title}`);
+});
+
+app.get('/article/:title/:category/:author', (req, res) => {
+  const { title, category, author } = req.params;
+
+  res.send(
+    `You are reading article with title : ${title}, category : ${category}, and author is ${author}`
+  );
+});
+
+app.get('*', (req, res) => {
+  res.send('Page not found');
+});
+
+app.listen(8001, () => {
+  console.log('Server is running on http://localhost:8001');
+});
