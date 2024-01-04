d7f69b9a3bc7d56c2ee4df2514f8abcfa9e555c5
mrmuhammadrifki <immuhammadrifki@gmail.com>
2024-01-05 00:23:37 +0700

    Interpolation Syntax Ejs

---
+++ express-ejs/interpolation-syntax-ejs/index.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(8001, () => {
+  console.log(`listening on host http://localhost:8001`);
+});
