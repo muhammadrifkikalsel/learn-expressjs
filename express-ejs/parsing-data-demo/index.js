10ac8ccd853265f7c30ab61a5eadfa16dc0a637f
mrmuhammadrifki <immuhammadrifki@gmail.com>
2024-01-06 07:28:44 +0700

    Parsing Data Demo

---
+++ express-ejs/parsing-data-demo/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/t/:tag', (req, res) => {
+  const { tag } = req.params;
+  res.render('tag', { tag });
+});
+
+app.get('/rand', (req, res) => {
+  const num = Math.floor(Math.random() * 20) + 1;
+  res.render('random', { random: num });
+});
+
+app.listen(8001, () => {
+  console.log(`listening on host http://localhost:8001`);
+});
