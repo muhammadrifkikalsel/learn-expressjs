e6818294f5682ebbce6428404174b75697a843fe
mrmuhammadrifki <immuhammadrifki@gmail.com>
2024-01-07 17:01:19 +0700

    Defines Route Get_Post

---
+++ express-restful-api/defines-route-get-post/rest/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/order', (req, res) => {
+  res.send('GET order response');
+});
+
+app.post('/order', (req, res) => {
+  res.send('GET order response');
+});
+
+app.listen('8001', () => {
+  console.log(`Server is running on : http://localhost:8001`);
+});
